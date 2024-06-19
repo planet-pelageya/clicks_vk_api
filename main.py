1cdcf7960c7609bf298b866bf626f7eb6cda2b34
planet-pelageya <ryazancev_0101@mail.ru>
2024-06-19 03:19:55 +0300

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,9 @@ SHORT_LINK_METHOD = "https://api.vk.com/method/utils.getShortLink"
 lINK_INFO_METHOD = "https://api.vk.com/method/utils.getLinkStats"
 API_VERSION = "5.199"
 SHORT_LINK_MARKER = 'vk.cc'
-def shorten_link( url, ACCESS_TOKEN):
+
+
+def shorten_link(ACCESS_TOKEN, url):
     params = {
         "access_token": ACCESS_TOKEN,
         "v": API_VERSION,
@@ -21,9 +23,12 @@ def shorten_link( url, ACCESS_TOKEN):
             error_message = short_url["error"]["error_code"]
             return f"Ошибка: {error_message}"
         else:
-            return short_url["response"]['short_url']
+            short_url = short_url["response"]['short_url']
+            return f"Ваша ссылка: {short_url}"
     else:
         return f"Ошибка: {response.status_code}"
+
+
 def count_clicks(ACCESS_TOKEN, url):
     parsed_url = urllib.parse.urlparse(url)
     parts_of_link = parsed_url.path.split('/')
@@ -32,32 +37,35 @@ def count_clicks(ACCESS_TOKEN, url):
         "access_token": ACCESS_TOKEN,
         "v": API_VERSION,
         "key": key,
-        "interval":"forever"
+        "interval": "forever"
     }
-    response = requests.get(lINK_INFO_METHOD, params = params)
+    response = requests.get(lINK_INFO_METHOD, params=params)
     if response.ok:
         response_json = response.json()
         if "error" in response_json:
             error_message = response_json["error"]["error_code"]
-            print(f'Ошибка1 {error_message}')
+            return f"Ошибка: {error_message}"
         else:
             count_clicks = response_json["response"]['stats'][0]["views"]
-            print(f"количество кликов {count_clicks}")
+            return f"Колличество кликов:{count_clicks}"
     else:
-        print(f"Ошибка: {response.status_code}")
+        return f"Ошибка: {response.status_code}"
 
 
-def is_shorten_link(url, ACCESS_TOKEN):
+def is_shorten_link(ACCESS_TOKEN, url):
     if SHORT_LINK_MARKER in urllib.parse.urlparse(url).netloc:
-        count_clicks(ACCESS_TOKEN, url)
+        return True
     else:
-        shorten_link(ACCESS_TOKEN, url)
+        return False
 
 
 if __name__ == '__main__':
     url = input("введите ссылку ")
     load_dotenv()
     ACCESS_TOKEN = os.environ["ACCESS_TOKEN"]
-    count_clicks(url,ACCESS_TOKEN)
+    if is_shorten_link(ACCESS_TOKEN, url):
+        print(count_clicks(ACCESS_TOKEN, url))
+    else:
+        print(shorten_link(ACCESS_TOKEN, url))
 
 
